3a271ea7f78ffa303fa3020a16eb4a186b94a853
Hugo Guerra <hugoguerra205@gmail.com>
2026-08-05 11:40:07 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,19 @@
+variable = 1
+
+print(type(variable))
+
+variable = 1.1
+
+print(type(variable))
+
+variable = False
+
+print(type(variable))
+
+variable = "Hi"
+
+print(type(variable))
+
+variable = []
+
+print(type(variable))
